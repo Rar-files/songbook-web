02b954e81d43ec5c1e3641f563c5977cab12b9bc
rar-files <dwidlak@outlook.com>
2023-12-27 03:51:56 +0100

    ChangetoGetAllSongsInSongPage

--- src/app/song/[id]/page.tsx
+++ src/app/song/[id]/page.tsx
@@ -5,14 +5,13 @@ import { NextPage } from 'next'
 import useSWR from 'swr'
 
 const SongPage: NextPage<{ params: { id: string } }> = ({ params }) => {
-    const { data, error, isLoading } = useSWR(
-        `/api/song/${params.id}`,
-        (...args) => fetch(...args).then((res) => res.json())
+    const { data, error, isLoading } = useSWR(`/api/song/`, (...args) =>
+        fetch(...args).then((res) => res.json())
     )
 
     if (error) return <div>failed to load</div>
     if (isLoading) return <div>loading...</div>
-    return <Song song={JSON.parse(data)} />
+    return <Song song={data[params.id]} />
 }
 
 export default SongPage
